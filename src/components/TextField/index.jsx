0a7af5a489c70509ddc798ea60902da79a5c4c7f
Nursultan <nshabaibekov@gmail.com>
2021-06-10 23:10:47 +0400

    initial commit

---
+++ src/components/TextField/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { Input } from "antd";
+import "./TextField.less";
+
+const Label = styled.label`
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 120%;
+  color: #565a65;
+  margin-bottom: 8px;
+`;
+
+const CustomInput = styled(Input)`
+  border-bottom: 1px solid #b6bfd9;
+  border-top: 0;
+  border-right: 0;
+  border-left: 0;
+  width: 200px;
+  border-radius: 0px;
+
+  &:focus {
+    box-shadow: none;
+  }
+  &::placeholder {
+    font-family: "Open Sans";
+    font-size: 16px;
+    line-height: 150%;
+    color: #dadfeb;
+  }
+`;
+export const TextField = ({ label, type, placeholder }) => {
+  return (
+    <div className="textfield">
+      <Label htmlFor="Your Name">{label}</Label>
+      <CustomInput type={type} name="" id="" placeholder={placeholder} />
+    </div>
+  );
+};
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextField } from "../../components/TextField";
+export const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <TextField placeholder="Your Name" label="Your Name" type="text" />
+    </div>
+  );
+};
